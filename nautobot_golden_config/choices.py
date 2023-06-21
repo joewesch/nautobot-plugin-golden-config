0a3880ed4bf852c2c70de9772fa874e400165faa
Joe Wesch <joe.wesch@networktocode.com>
2023-06-21 15:01:57 -0500

    Adds ConfigPlan model and initial generation utils

--- nautobot_golden_config/choices.py
+++ nautobot_golden_config/choices.py
@@ -14,3 +14,21 @@ class ComplianceRuleTypeChoice(ChoiceSet):
         (TYPE_JSON, "JSON"),
         (TYPE_CUSTOM, "CUSTOM"),
     )
+
+
+class ConfigPlanTypeChoice(ChoiceSet):
+    """Choiceset used by ConfigPlan."""
+
+    TYPE_INTENDED = "intended"
+    TYPE_MISSING = "missing"
+    TYPE_REMEDIATION = "remediation"
+    TYPE_MANUAL = "manual"
+    TYPE_FULL = "full"
+
+    CHOICES = (
+        (TYPE_INTENDED, "Intended"),
+        (TYPE_MISSING, "Missing"),
+        (TYPE_REMEDIATION, "Remediation"),
+        (TYPE_MANUAL, "Manual"),
+        (TYPE_FULL, "Full"),
+    )
